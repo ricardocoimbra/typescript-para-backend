d0d808283d4e6ddf8f6b133d4ad3e713df6289c4
Ricardo Coimbra - CURA <coimbra.rico@gmail.com>
2024-04-25 15:10:15 -0400

    Criando o TipoPet

--- src/controller/PetsController.ts
+++ src/controller/PetsController.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from "express";
+import type TipoPet from "../tipos/TipoPet";
 
-let listaPets= []
+//let listaPets: TipoPet[] = [];
+let listaPets: Array<TipoPet> = [];
+
 export default class PetsController {
     criaPet(req: Request, res: Response) {
-        const novoPet = req.body;
+        //const novoPet = req.body as TipoPet;
+        const { id, adotado, especie, idade, nome } = <TipoPet>req.body;
+        const novoPet: TipoPet = {id, adotado, especie, idade, nome}
         listaPets.push(novoPet);
         return res.status(201).json(novoPet);
     }
