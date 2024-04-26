e7784ae6a79fd9a8d493b74032e9773f53211c57
Ricardo Coimbra - CURA <coimbra.rico@gmail.com>
2024-04-26 13:59:41 -0400

    adicionando typeorm

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,23 @@
 import express, { Response } from "express";
-import router from "./routes"
+import router from "./routes";
+import "reflect-metadata";
+import { AppDataSource } from "../config/dataSource";
 
 const app = express();
 app.use(express.json());
 router(app);
 
+AppDataSource.initialize()
+    .then(()=> {
+      console.log("Banco de Dados conectado.")
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+;
+
 app.get("/", (_, res: Response) => {
   res.send("Bem vindo ao curso de TypeScript!");
 });
-
-// function criaPet(id: number, nome: string, especie: string, idade: number, adotado: boolean) {
-//   return {
-//     id,
-//     nome,
-//     especie,
-//     idade,
-//     adotado,
-//   };
-// }
-
-// let id: number = 0;
-// function geraId() {
-//   id = id + 1;
-//   return id;
-// }
-
-// app.post("/pets", (_, res: Response) => {
-//   const pet1 = criaPet(geraId(), "Bolt", "cachorro", 3, false);
-//   const pet2 = criaPet(geraId(), "Mel", "gato", 2, false);
-
-//   res.send([pet1, pet2]);
-// });
 
 export default app;
