fc777f37b8f64de3dfcd7c7afc6753d6d28803df
Ricardo Coimbra - CURA <coimbra.rico@gmail.com>
2024-04-29 16:18:36 -0400

    criando a entidade pet

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Response } from "express";
 import router from "./routes";
 import "reflect-metadata";
-import { AppDataSource } from "../config/dataSource";
+import { AppDataSource } from "./config/dataSource";
 
 const app = express();
 app.use(express.json());
